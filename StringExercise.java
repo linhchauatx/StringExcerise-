e3269e325af8aa5c69552a6ed24c85ae2f9af726
Chau Hong Linh <chauhonglinh@gmail.com>
2019-12-06 21:15:06 -0600

    Fix bugs.

--- StringExercise.java
+++ StringExercise.java
@@ -6,25 +6,24 @@ import java.util.Scanner;
 
 public class StringExercise {
     public static void main (String [] args) {
-
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("enter a string: ");
-        String stringInput = scanner.next();
+        String stringInput = scanner.nextLine();
 
         System.out.print("enter a character: ");
-        String characterInput = scanner.next();
-        scanner.nextLine();
+        String characterInput = scanner.nextLine();
 
-
-        System.out.println("The number of times the character appeared in the String: " + countTheWantedCharacter(stringInput,characterInput) );
+        System.out.println("The number of times the character " +
+            characterInput +
+            " appeared in the String " + stringInput + ": " +
+            countTheWantedCharacter(stringInput,characterInput) );
 
         countDifferentCharacters(stringInput);
     }
 
     public static int countTheWantedCharacter(String stringInput, String characterInput) {
-
-        String arr[] = stringInput.split(",");
+        String arr[] = stringInput.split("");
         int count = 0;
 
         for(String a : arr) {
@@ -33,24 +32,32 @@ public class StringExercise {
 
             }
         }
-
         return count;
     }
 
     public static void countDifferentCharacters (String stringInput) {
         String [] arr = stringInput.split("");
-        Arrays.sort(arr);
+
+        // Arrays.sort has no effect with array of Stringss - don't need this
+        // Strings Arrays.sort(arr);
 
         ArrayList<String> arr1 = new ArrayList<>();
-        int count = 1;
+
+        // At the beginning, we have zero character
+        int count = 0;
 
         for (int i = 0; i < arr.length; i++) {
-
-            if ((i != 0) && (!(arr[i].equals(arr[i-1])))) {
+            // Always count the first char
+            if (i == 0) {
+              count++;
+              arr1.add(arr[i]);
+            }
+            // if current char is different than the previous, count it
+            else if (!(arr[i].equals(arr[i-1]))) {
                count++;
-               arr1.add(arr[i-1]);
+               arr1.add(arr[i]);
 
-          }
+            }
         }
 
         System.out.println("The number of different characters in the String: " + count);
